82577c7b4adc62edd1a8187a40146f8289f271b2
Gabriel Khachatryan <gmkhach@gmail.com>
2020-09-08 22:48:38 -0400

    documentation example

---
+++ documentationInGo.go
@@ -0,0 +1,11 @@
+// In Go documentation is built right into the comments.
+// If you type "go doc" in the terminal where this directory is you will see these two lines for package main
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    
+}

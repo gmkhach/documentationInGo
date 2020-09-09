6ca9faa14dcb35574651db130b4f270bd5c299ab
Gabriel Khachatryan <gmkhach@gmail.com>
2020-09-08 23:03:51 -0400

    adding documentation to funcs

--- documentationInGo.go
+++ documentationInGo.go
@@ -1,5 +1,5 @@
 // In Go documentation is built right into the comments.
-// If you type "go doc" in the terminal where this directory is you will see these two lines for package main
+// These two lines will print in your terminal if you navigate to the directory where this file is and type-in the "go doc" command.
 package main
 
 import (
@@ -7,5 +7,12 @@ import (
 )
 
 func main() {
-    
+	a, b := 21, 21
+	fmt.Println(IntSum(a, b))
+}
+
+// Sum takes two integers and reterns an integer (the sum of the two arguments).
+// Type-in "go doc IntSum" in the terminal to read this comment, as well as the func signature.
+func IntSum(a, b int) int {
+	return a + b;
 }
